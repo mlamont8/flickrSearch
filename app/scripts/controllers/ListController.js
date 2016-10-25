3441d0d31b34eaac014fcee20c295811f8933143
mlamont8 <debiandmiat@hotmail.com>
2016-10-24 21:48:04 -0400

    minor update

--- app/scripts/controllers/ListController.js
+++ app/scripts/controllers/ListController.js
@@ -1,12 +1,16 @@
 'use strict';
 angular.module('flickrsearchApp')
-.controller('ListController', ['$scope', '$http',function($scope, $http){
-   $scope.results = [];
+.controller('ListController', ['$scope', '$http', 'angularGridInstance',function($scope, $http, angularGridInstance){
+
    $scope.Searching = false;
    $scope.firstTime = true;
     $scope.search = function(){
+        $scope.results = [];
         $scope.firstTime=false;
         $scope.Searching = true;
+        $scope.refresh = function(){
+            angularGridInstance.gallery.refresh();   
+     };
         $http({
             method: 'GET',
             url: 'https://api.flickr.com/services/rest',
@@ -14,12 +18,16 @@ angular.module('flickrsearchApp')
                 method: 'flickr.photos.search',
                 api_key: '1dfbed40745d1f76dc935c78c30abf16',
                 text: $scope.searchTerm,
+                safe_search: '1',
+                per_page: '50',
                 format: 'json',
                 nojsoncallback: 1
             }
         }).then(function successCallback(getback){
             $scope.results = getback;
             $scope.Searching = false;
+            $scope.searchTerm = "";
+            console.log($scope.results);
 
         },function errorCallback() {
             console.error('error');
@@ -27,5 +35,5 @@ angular.module('flickrsearchApp')
         }
     );
     };  
-     
+
  }]);
